edd93ea6eafd9d5237f5e8c33bfb73f1ebef0e1a
c3479858 <c3479858@uon.edu.au>
2025-03-21 15:13:13 +1100

    Update Q1b.py

--- Programming Assignment 1/Q1b.py
+++ Programming Assignment 1/Q1b.py
@@ -6,11 +6,10 @@ c =  -3
 x = np.linspace(-5, 5, num=1001)            #list of 1001 values from -5 to 5
 
 y = a*x**2 + b*x + c                        #equation
-print("y =", y)                             #printing answers
 
 plt.plot(x, y, "r")                         #plots the plot with a red line
 plt.xlabel("x")
 plt.ylabel("y", rotation=0)
-plt.grid("on")                              #not sure why I have a warning here :/
+plt.grid()
 plt.legend(["y versus x"])
 plt.show()                                  #displays the plot
