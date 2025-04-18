635111c79e2849e9ff36248d68004d8b99701178
c3479858 <c3479858@uon.edu.au>
2025-04-18 22:21:23 +1000

    Programming Assignment Complete

--- Programming Assignment 1/Q6.py
+++ Programming Assignment 1/Q6.py
@@ -5,7 +5,7 @@ k = [1, 2, 3, 4, 5, 6]                                      #all values for k
 
 for i in k:
     counter = 0
-    for _ in range(1, N + 1):
+    for j in range(1, N + 1):
         die = np.random.randint(1, 7, size=dice)            #rolls 2 dice into an array
         if max(die) < i:                                    #checks if the max number in the array is less than k
             counter += 1                                    # since if the max is less than k, both dice must have face
--- Programming Assignment 1/Q8.py
+++ Programming Assignment 1/Q8.py
@@ -1,1 +1,11 @@
-def pascal_rows(rows):
+from math import factorial
+
+length = 3                                                                      #amount of rows wanted
+
+for n in range(length):                                                         #splitting length into individual rows
+    for i in range(length-n+1):                                                 #left side spacing
+        print(end=" ")
+
+    for r in range(n+1):                                                        #setting up nCr eqn
+        print(factorial(n) // (factorial(r) * factorial(n-r)), end=" ")         #nCr = n!/(r!*(n-r)!) and right spacing
+    print()                                                                     #new row
