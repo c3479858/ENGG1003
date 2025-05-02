6c89eaefec397930eb61804626a2dd8299c51b7f
c3479858 <c3479858@uon.edu.au>
2025-05-03 02:14:44 +1000

    Week 8 Start

---
+++ Week 8/bisection.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def f(x):
+    return np.exp(x) - 3*x
+
+eps = 1e-6
+xLO = 1
+xHI = 2
+
+xMID = (xLO + xHI) / 2
+iters = 0
+while abs(f(xMID)) > eps:
+    if f(xMID) * f(xHI) < 0:
+        xLO = xMID
+    else:
+        xHI = xMID
+    xMID = (xLO + xHI) / 2
+    iters += 1
+
+print("Solution: {:.5f}".format(xMID))
+print("Number of iterations: {}".format(iters))
+print("Check: f({:.5f}) = {:.5f}".format(xMID, f(xMID)))
---
+++ Week 8/secant.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def f(x):
+    return np.exp(x) - 3*x
+
+eps = 1e-6
+x0 = 0
+x1 = 1
+iters = 0
+while abs(f(x1)) > eps:
+    x = x1 - f(x1)*((x1 - x0)/(f(x1) - f(x0)))
+    x0 = x1
+    x1 = x
+    iters += 1
+
+print("Soln: {}".format(x))
+print("Iterations: {}".format(iters))
+print("Check: f({:.8f}) = {:.6f}".format(x, f(x)))
