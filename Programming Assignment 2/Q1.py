c3c85414ff04429ad7bc8f876791457f5f035937
c3479858 <c3479858@uon.edu.au>
2025-06-01 12:02:41 +1000

    Update Q1.py

--- Programming Assignment 2/Q1.py
+++ Programming Assignment 2/Q1.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import rootfinding as rf
 
-#NEED TO RECHECK AND ADD COMMENTS BUT IT ALL SEEMS TO WORK, HAVEN'T IMPLEMENTED EDGE CASE WHERE THE RESULT OF B IS NEGATIVE
+#NEED TO RECHECK AND ADD COMMENTS BUT IT ALL SEEMS TO WORK
+#HAVEN'T IMPLEMENTED EDGE CASE WHERE THE RESULT OF B IS NEGATIVE, CAN'T BE ASSED TBH
 
 def f(x):                                                                           #Part A function
     return (x**2) * (np.sin(x)) + (2*x) - 3
@@ -14,7 +15,7 @@ def g(h, vTgt = 4, g = 9.81, L = 5):                                            
 a1 = 0
 b1 = 2
 x = np.linspace(a1, b1, num=1000)                                                   #1000 points of x between 0 and 2
-xBi, iters0 = rf.bisection(f, a1, b1)
+xBi, iters0 = rf.bisection(f, a1, b1)                                                                   #for graphing
 xSe, iters1 = rf.secant(f, a1, b1)
 
 a2 = 1
@@ -23,7 +24,7 @@ h, iters2 = rf.secant(g, a2, b2)
 
 print("a)\n   i) {:.5f} when subbed back into the eqn = {:.5f} (Bisection Method)".format(xBi, f(xBi)))
 print("  ii) {:.5f} when subbed back into the eqn = {:.5f} (Secant Method)".format(xSe, f(xSe)))
-print("b) h = {:.2f}".format(h))
+print("b)    h = {:.2f}".format(h))
 
 plt.plot(x, f(x))
 plt.xlabel("x")
