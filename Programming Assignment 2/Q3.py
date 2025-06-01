fafe1db6cc073770ec51dd1c4bbbd020b6f3d218
c3479858 <c3479858@uon.edu.au>
2025-06-01 12:52:39 +1000

    Q3 start

--- Programming Assignment 2/Q3.py
+++ Programming Assignment 2/Q3.py
@@ -1,1 +1,20 @@
-#REALLY NEED TO START, LEARN CSV READING FIRST!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+#ADD COMMENTS THEN SUBMIT
+#i)
+t = np.linspace(0, 100, num=11)
+y = np.array([132, 138, 189, 229, 317, 304, 435, 435, 506, 545, 629])
+plt.plot(t, y, "ro")
+
+#ii)
+f = interp1d(t, y)
+yInterp = f(t)
+plt.plot(t, yInterp, "b-")
+
+#iii)
+t55 = 55
+y55 = f(t55)
+#plt.plot(t55, y55, "go")
+print("The interpolated value at t = 55 is {}".format(y55))
+plt.show()
